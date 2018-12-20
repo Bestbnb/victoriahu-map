5aa51b7aac93eacbf2a4c4b3d64a769c1ebf3284
Victoria Hu <vh12490@gmail.com>
2018-12-20 15:51:16 -0800

    add db

---
+++ db/index.js
@@ -0,0 +1,11 @@
+const mysql = require('mysql');
+const mysqlConfig = require('./config.js');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'student',
+    database: 'bestbnb'
+});
+
+module.exports = connection;
---
+++ schema.sql
@@ -0,0 +1,45 @@
+DROP DATABASE IF EXISTS bestbnb;
+CREATE DATABASE bestbnb;
+USE bestbnb;
+
+DROP TABLE IF EXISTS map;
+		
+CREATE TABLE map (
+  id INTEGER NOT NULL AUTO_INCREMENT,
+  name VARCHAR(50),
+  lat INTEGER,
+  lng INTEGER,
+  PRIMARY KEY (id)
+);
+
+DROP TABLE IF EXISTS recommendations;
+		
+CREATE TABLE recommendations (
+  rec_id INTEGER,
+  name VARCHAR(50),
+  lat INTEGER,
+  lng INTEGER,
+  url VARCHAR(50),
+  stars INTEGER,
+  label INTEGER,
+  color VARCHAR(50),
+  map_id INTEGER,
+  PRIMARY KEY (rec_id),
+  FOREIGN KEY (map_id) REFERENCES map(id)
+);
+
+-- ---
+-- Table Properties
+-- ---
+
+-- ALTER TABLE `map` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `recommendations` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- ---
+-- Test Data
+-- ---
+
+-- INSERT INTO `map` (`id`,`name`,`lat`,`lng`) VALUES
+-- ('','','','');
+-- INSERT INTO `recommendations` (`id`,`rec_id`,`name`,`lat`,`lng`,`url`,`stars`,`label`,`color`) VALUES
+-- ('','','','','','','','','');
