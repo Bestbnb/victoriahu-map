c048855de6d461f89e65f64d516da9e133245f2a
Victoria Hu <vh12490@gmail.com>
2018-12-19 17:43:59 -0800

    setup

---
+++ server/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, './../client/dist/index.html')));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, './../client/dist/index.html')));
+
+app.listen(port, () => console.log(`Map component listening on port ${port}!`));
