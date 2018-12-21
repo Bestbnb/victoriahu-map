0ce8a3667ba9871d6cc22bb3e61555cf08bc8c45
Victoria Hu <vh12490@gmail.com>
2018-12-21 11:57:42 -0800

    add get request to /map

--- server/index.js
+++ server/index.js
@@ -2,8 +2,19 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const path = require('path');
+const db = require('./../db/index.js');
 
 app.use(express.static(path.join(__dirname, './../client/dist/')));
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, './../client/dist/index.html')));
 
+app.get('/map', (req, res) => {
+    var random = Math.floor(Math.random() * 100) + 1  
+    var sql = 'select * from map where id = (?)';
+    db.query(sql, [random], (err, data) => {
+        if (err) {
+            throw err;
+        }
+        res.send(data);
+    })
+})
 app.listen(port, () => console.log(`Map component listening on port ${port}!`));
